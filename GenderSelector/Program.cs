2ea93d5f83f162168d85e2e137c4aca6bcd1078d
Farzad <97603713+farzad-d@users.noreply.github.com>
2024-03-08 17:27:16 +0330

    Add files via upload

---
+++ GenderSelector/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenderSelector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gender;
+            Console.WriteLine("Are you female? true/false");
+            gender=bool.Parse(Console.ReadLine());
+            
+            if (gender==true)
+            {
+                Console.WriteLine("You are female");
+            }
+            else
+            {
+                Console.WriteLine("You are male");
+            }
+        }
+    }
+}
---
+++ GradesAverage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GradesAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sum of grades
+            double totalGrades = 0;
+
+            // Adding the grades
+            for(int i = 0; i < 4; i++)
+            {
+                Console.Write($"{i+1}.Enter the grade: ");
+                totalGrades += Convert.ToDouble(Console.ReadLine());
+            }
+            // Display the result
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"The average of grades is: {totalGrades / 4}");
+        }
+    }
+}
