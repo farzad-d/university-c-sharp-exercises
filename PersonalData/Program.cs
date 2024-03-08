3130073f95c79dbccaa88a63b83f00b84baf09c6
Farzad <97603713+farzad-d@users.noreply.github.com>
2024-03-08 17:28:27 +0330

    Add files via upload

---
+++ PersonalData/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PersonalData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console colors
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
+
+            // Show personal information
+            Console.WriteLine("** ** ** ** ** ** ** ** ** ** ** ** **");
+            Console.WriteLine();
+            Console.WriteLine("   Name: Farzad");
+            Console.WriteLine("   Family: Daneshpour");
+            Console.WriteLine("   Birth: 14/7/1990");
+            Console.WriteLine("   Job: Collage student");
+            Console.WriteLine();
+            Console.WriteLine("** ** ** ** ** ** ** ** ** ** ** ** **");
+
+            // If Enter is pressed clear the console, then show the phone number
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("** ** ** ** ** ** ** ** ** ** ** ** **");
+            Console.WriteLine();
+            Console.WriteLine("   Phone number: 0916 066 1091");
+            Console.WriteLine();
+            Console.WriteLine("** ** ** ** ** ** ** ** ** ** ** ** **");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProductInfo/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ProductInfo
+{
+    class program {         
+        static void Main(string[] args)
+        {
+            // Variables
+            string name;
+            float weight;
+            int price;
+            bool type;
+
+            // Inputs
+            Console.WriteLine("Enter the product name: ");
+            name=Console.ReadLine();
+            Console.WriteLine("Enter the product weight: ");
+            weight=float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the price of the product: ");
+            price=int.Parse(Console.ReadLine());
+            Console.WriteLine("Is that fragile? True/False");
+            type=bool.Parse(Console.ReadLine());
+
+            // Color scheme
+            Console.BackgroundColor=ConsoleColor.DarkBlue;
+            Console.ForegroundColor=ConsoleColor.White;
+            Console.Clear();
+
+            // Outputs
+            Console.WriteLine("\n** ** ** ** ** ** ** ** ** ** ** ** ** ** **\n");
+            Console.WriteLine("  Product name: " + name);
+            Console.WriteLine("  Product weight: " + weight + "kg");
+            Console.WriteLine("  Price: " + price + "$");
+            Console.WriteLine("  Fragile: " + type);
+            Console.WriteLine("\n** ** ** ** ** ** ** ** ** ** ** ** ** ** **\n");
+
+        }
+    }
+}
