8a582d34cb3e1d5ae89f7d6f80b93e84922fe4be
Farzad <97603713+farzad-d@users.noreply.github.com>
2024-03-08 17:26:51 +0330

    Add files via upload

---
+++ ColorSelector/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ColorSelector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vars
+            string name;
+            char color;
+
+            //Name input
+            Console.WriteLine("Type your name: ");
+            name=Console.ReadLine();
+
+            // Color input
+            Console.WriteLine("Select color: r(Red), b(Blue), m(Magenta)");
+            color=char.Parse(Console.ReadLine());
+
+            // Display the result
+            if (color=='r')
+            {
+                Console.BackgroundColor=ConsoleColor.Red;
+            }
+            else if (color=='b')
+            {
+                Console.BackgroundColor=ConsoleColor.Blue;
+            }
+            else if (color=='m')
+            {
+                Console.BackgroundColor=ConsoleColor.Magenta;
+            }
+            else
+            {
+                Console.BackgroundColor=ConsoleColor.Yellow;
+            }
+
+            Console.Clear();
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ Divisibility/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Divisibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Getting numbers from the user
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            // Calculate and display the result
+            for(int i = num1; i<num2; i++)
+            {
+                // if(i%3==0 || i%7==0)
+                if(i%3==0 && i%7==0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
